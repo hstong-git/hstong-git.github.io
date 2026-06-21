b18557a679af42df9091c2d2d1ee988c7ac757fa
nouxiaxia <nouxiaxia@outlook.com>
2026-06-21 15:31:42 +0800

    feat: 配置GitHub Actions自动化部署

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    base: '/blog',
-    site: 'https://hstong-git.github.io'
+    site: 'https://hstong-git.github.io',
+    base: '/blog'
 });
