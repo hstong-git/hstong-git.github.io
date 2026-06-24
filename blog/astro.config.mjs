999ee7bf969248c7cb48e5daee6cb2514eca061b
nouxiaxia <nouxiaxia@outlook.com>
2026-06-24 20:46:09 +0800

    feat: 更新了域名

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://hstong-git.github.io'
+    site: 'https://huang-sitong.github.io'
 });
